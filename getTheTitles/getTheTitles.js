49b76cadb5a60af1c12414f9ad534fdd01f1f9ab
Kaiying Fu <kaiying.fu@gmail.com>
2021-04-07 15:49:53 +0800

    Complete Exercise GetTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
 const getTheTitles = function() {
-
+  const array = arguments[0];
+  // Each element in array represents a single book with a title and author
+  // Map over each element to obtain just the titles
+  return array.map(book => book.title);
 }
 
 module.exports = getTheTitles;
