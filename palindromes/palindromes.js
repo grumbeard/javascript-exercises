3e50ba9111c490c6aa51b93bcf35acf068d59e28
Kaiying Fu <kaiying.fu@gmail.com>
2021-04-07 15:23:55 +0800

    Complete Exercise Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
 const palindromes = function() {
-
+  const string = arguments[0];
+  charArr = string.match(/\w{1}/g);
+  for (let i = 0; i <= (charArr.length /2); i++) {
+    let charX = charArr[i].toLowerCase();
+    let charY = charArr[charArr.length - i - 1].toLowerCase();
+    if (charX != charY) return false;
+  }
+  return true;
 }
 
 module.exports = palindromes
