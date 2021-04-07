e87382b958bc5998c43571d7b0e1c2d57a5ee345
Kaiying Fu <kaiying.fu@gmail.com>
2021-04-07 15:45:43 +0800

    Complete Exercise Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = function() {
-
+  // sample sequence: 1,1,2,3,5,8,13,21,34,56,90,146,236,382
+  let num = +arguments[0];
+  if (num < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  let c = 0;
+  for (let i = 0; i < num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return a;
 }
 
 module.exports = fibonacci
