335f04167da16e2dba1d903c0a07a413b3349d00
Kaiying Fu <kaiying.fu@gmail.com>
2021-04-07 16:18:50 +0800

    Complete Exercise FindTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
 let findTheOldest = function() {
+  people = arguments[0];
+  let oldestPerson = people.reduce((currentPerson, nextPerson) => {
+    let ageCurrent = 0;
+    let ageNext = 0;
 
+    if (currentPerson.yearOfDeath) {
+      ageCurrent = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    } else {
+      ageCurrent = new Date().getFullYear() - currentPerson.yearOfBirth;
+    }
+    if (nextPerson.yearOfDeath) {
+      ageNext = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+    } else {
+      ageNext = new Date().getFullYear() - nextPerson.yearOfBirth;
+    }
+    return ageCurrent > ageNext ? currentPerson : nextPerson;
+  });
+  return oldestPerson;
 }
 
 module.exports = findTheOldest
